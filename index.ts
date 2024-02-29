9761798f5a3cf0194876ab0a91600fc10b175ca7
nikola04 <nikolanedeljkovic.official@gmail.com>
2024-02-29 15:13:56 +0000

    Navigation Menu and Routes for main page

--- index.ts
+++ index.ts
@@ -1,5 +1,16 @@
 import express from 'express'
+import env from 'dotenv'
 
+env.config()
 const app = express()
 
-app.listen(process.env.PORT)
+app.set('views', 'src/views')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('🔥 Server started at Port:', process.env.PORT)
+})
