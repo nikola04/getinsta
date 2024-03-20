8a4e7a0912244aa524dbecb6e2759cb73150e8a7
nikola04 <nikolanedeljkovic.official@gmail.com>
2024-03-20 04:09:07 +0000

    Added: Importing regex from constant & Updating User prefrences

--- src/routes/apis/account.ts
+++ src/routes/apis/account.ts
@@ -1,9 +1,8 @@
 import { Router, Request, Response, urlencoded, json } from 'express';
 import { rateLimit } from '../../middlewares/ratelimit';
-import { validateCaptchaToken, validateCatpchaResponse } from '../../functions/recaptcha';
 import User from '../../../schemas/user';
+import { EmailRegex } from '../../constants/regex';
 
-const EmailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 const router = Router()
 
 router.use(json())
@@ -40,10 +39,14 @@ router.patch('/email', rateLimit({
     }
 }, false), async (req: Request, res: Response) => {
     try{
-        const email: null|string = req.body?.email
-        if(!email || !EmailRegex.test(email)) return res.status(400).json({ error: 1, message: 'Valid Email is not provided.' })
+        const _email: null|string = req.body?.email
+        if(!_email) return res.status(400).json({ error: 1, message: 'Email is not provided.' })
+        const email = _email.trim()
+        if(!EmailRegex.test(email)) return res.status(400).json({ error: 1, message: 'Provided Email is valid.' })
         if(!req.signedIn || !req.user) return res.status(401).json({ error: 2, message: 'User is not logged in.' })
-        await User.updateOne({ _id: req.user._id }, { $set: { email, verified_email: false }})
+        // Check if user is already using that mail
+        const verified_email = req.user.google?.email === email
+        await User.updateOne({ _id: req.user._id }, { $set: { email, verified_email }})
         return res.sendStatus(200)
     }catch(err){
         console.error(err)
@@ -51,6 +54,14 @@ router.patch('/email', rateLimit({
     }
 })
 
+const validateName = (firstName: string, lastName: string): Boolean => {
+    if(!firstName || !lastName) return false
+    firstName = firstName.trim()
+    lastName = lastName.trim()
+    if(firstName.length == 0 || firstName.length > 36) return false
+    if(lastName.length == 0 || lastName.length > 36) return false
+    return true
+}
 router.patch('/name', rateLimit({
     endpoint: '/account/name',
     rateLimits: {
@@ -62,10 +73,52 @@ router.patch('/name', rateLimit({
     }
 }, false), async (req: Request, res: Response) => {
     try{
-        const name: null|string = req.body?.name
-        if(!name || name.length < 2 || name.length > 32) return res.status(400).json({ error: 1, message: 'Valid Name is not provided.' })
+        const firstName: null|string = req.body?.first_name
+        const lastName: null|string = req.body?.last_name
+        if(!validateName(firstName, lastName)) return res.status(400).json({ error: 1, message: 'Valid Name is not provided.' })
         if(!req.signedIn || !req.user) return res.status(401).json({ error: 2, message: 'User is not logged in.' })
+        const name = ({
+            first_name: firstName.trim(),
+            last_name: lastName.trim()
+        })
         await User.updateOne({ _id: req.user._id }, { $set: { name }})
+        return res.sendStatus(200)
+    }catch(err){
+        console.error(err)
+        return res.status(500).json({ error: 0, message: String(err) })
+    }
+})
+
+function isStringTrueFalse(string: null|string): Boolean{
+    if(!string || (string != 'false' && string != 'true')) return false;
+    return true
+}
+
+router.patch('/settings', rateLimit({
+    endpoint: '/account/name',
+    rateLimits: {
+        loggedIn: {
+            time: 1000,
+            limit: 1
+        },
+        anonymous: null
+    }
+}, false), async (req: Request, res: Response) => {
+    try{
+        const _securityEmails: null|string = req.body?.security_emails
+        const _newsletterEmails: null|string = req.body?.newsletter_emails
+        const _saveDownloads: null|string = req.body?.save_downloads
+        if(!isStringTrueFalse(_securityEmails) || !isStringTrueFalse(_newsletterEmails) || !isStringTrueFalse(_saveDownloads)) return res.status(400).json({ error: 1, message: 'Provide valid body params.' })
+        if(!req.signedIn || !req.user) return res.status(401).json({ error: 2, message: 'User is not logged in.' })
+
+        const security_emails = _securityEmails === 'true' ? true : false
+        const newsletter_emails = _newsletterEmails === 'true' ? true : false
+        const save_downloads = _saveDownloads === 'true' ? true : false
+        const email_preferences = {
+            security_emails,
+            newsletter_emails
+        }
+        await User.updateOne({ _id: req.user._id }, { $set: { email_preferences, save_downloads }})
         return res.sendStatus(200)
     }catch(err){
         console.error(err)
