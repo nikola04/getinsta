176454be6cb740d73e5ecfa343ab49eb838cf0d7
nikola04 <nikolanedeljkovic.official@gmail.com>
2024-03-20 04:07:27 +0000

    Added: Edit Data page routes & Downloads page route

--- src/routes/account.ts
+++ src/routes/account.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import { rateLimit } from '../middlewares/ratelimit';
-import ytdl from 'ytdl-core';
 const router = Router()
 
 router.get('/', rateLimit({
@@ -10,14 +9,51 @@ router.get('/', rateLimit({
             time: 1000,
             limit: 5
         },
-        anonymous: {
-            time: 500,
-            limit: 10
-        }
+        anonymous: null
     }
 }), (req, res) => {
     if(!req.signedIn) return res.redirect('/')
     res.render('account.ejs', { page: 'info', googleId: process.env.G_CLIENT_ID, googleRecaptchaKey: process.env.RECAPTCHA_KEY, signedIn: req.signedIn, user: req.user })
+})
+
+router.get('/edit/name', rateLimit({
+    endpoint: '/',
+    rateLimits: {
+        loggedIn: {
+            time: 1000,
+            limit: 5
+        },
+        anonymous: null
+    }
+}), (req, res) => {
+    if(!req.signedIn) return res.redirect('/')
+    res.render('account.ejs', { page: 'info/edit/name', googleId: process.env.G_CLIENT_ID, googleRecaptchaKey: process.env.RECAPTCHA_KEY, signedIn: req.signedIn, user: req.user })
+})
+router.get('/edit/email', rateLimit({
+    endpoint: '/',
+    rateLimits: {
+        loggedIn: {
+            time: 1000,
+            limit: 5
+        },
+        anonymous: null
+    }
+}), (req, res) => {
+    if(!req.signedIn) return res.redirect('/')
+    res.render('account.ejs', { page: 'info/edit/email', googleId: process.env.G_CLIENT_ID, googleRecaptchaKey: process.env.RECAPTCHA_KEY, signedIn: req.signedIn, user: req.user })
+})
+router.get('/edit/picture', rateLimit({
+    endpoint: '/',
+    rateLimits: {
+        loggedIn: {
+            time: 1000,
+            limit: 5
+        },
+        anonymous: null
+    }
+}), (req, res) => {
+    if(!req.signedIn) return res.redirect('/')
+    res.render('account.ejs', { page: 'info/edit/picture', googleId: process.env.G_CLIENT_ID, googleRecaptchaKey: process.env.RECAPTCHA_KEY, signedIn: req.signedIn, user: req.user })
 })
 
 router.get('/settings', rateLimit({
@@ -27,10 +63,7 @@ router.get('/settings', rateLimit({
             time: 1000,
             limit: 5
         },
-        anonymous: {
-            time: 500,
-            limit: 10
-        }
+        anonymous: null
     }
 }), (req, res) => {
     if(!req.signedIn) return res.redirect('/')
@@ -44,14 +77,24 @@ router.get('/subscriptions', rateLimit({
             time: 1000,
             limit: 5
         },
-        anonymous: {
-            time: 500,
-            limit: 10
-        }
+        anonymous: null
     }
 }), (req, res) => {
     if(!req.signedIn) return res.redirect('/')
     res.render('account.ejs', { page: 'subscriptions', googleId: process.env.G_CLIENT_ID, googleRecaptchaKey: process.env.RECAPTCHA_KEY, signedIn: req.signedIn, user: req.user })
+})
+router.get('/downloads', rateLimit({
+    endpoint: '/',
+    rateLimits: {
+        loggedIn: {
+            time: 1000,
+            limit: 5
+        },
+        anonymous: null
+    }
+}), (req, res) => {
+    if(!req.signedIn) return res.redirect('/')
+    res.render('account.ejs', { page: 'downloads', googleId: process.env.G_CLIENT_ID, googleRecaptchaKey: process.env.RECAPTCHA_KEY, signedIn: req.signedIn, user: req.user })
 })
 
 
