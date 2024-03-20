a90023df58283a36b40eb3f8829bb636ce7d4de0
nikola04 <nikolanedeljkovic.official@gmail.com>
2024-03-20 04:03:28 +0000

    Added: Events for updating user data & Prefrence Popup events

--- public/assets/js/account.ts
+++ public/assets/js/account.ts
@@ -1,3 +1,12 @@
+interface SettingsPreferences{
+    newsletter_emails?: boolean,
+    security_emails?: boolean,
+    save_downloads?: boolean
+}
+// @ts-ignore
+const original_preferences = window.accountPreferences as SettingsPreferences
+let current_preferences = { ...original_preferences }
+
 // Changes page loading blur state
 let page_loading = false
 const startPageLoading = () => {
@@ -41,6 +50,62 @@ const parseAccountPage = (html: Element): void => {
 }
 
 /**
+ * Email validation
+ * @returns True if email is valid or false when not
+ */
+function isValidEmail (email: string) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+}
+
+/**
+ * Add events when new page is loaded
+ */
+const attachEventsOnPageLoad = () => {
+    document.querySelectorAll('a.navigation-button').forEach((el) => el.addEventListener('click', e => {
+        e.preventDefault()
+        const element = el as HTMLAnchorElement
+        const page_url = element.href
+        changePageToUrl(page_url)
+    }))
+    document.querySelector('form#updateNameData')?.addEventListener('submit', e => {
+        e.preventDefault()
+        const fNameEl = document.querySelector('input#firstNameInput') as null|HTMLInputElement
+        const lNameEl = document.querySelector('input#lastNameInput') as null|HTMLInputElement
+        if(!fNameEl || !lNameEl) return
+        const first_name = fNameEl.value.trim(), last_name = lNameEl.value.trim()
+        if(first_name.length == 0 || first_name.length > 36) return
+        if(last_name.length == 0 || last_name.length > 36) return
+        fetch('/api/account/name', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ first_name, last_name })
+        }).then(res => {
+            if(res.ok) changePageToUrl('/account/')
+            else console.error(res.status)
+        })
+    })
+    document.querySelector('form#updateEmailData')?.addEventListener('submit', e => {
+        e.preventDefault()
+        const emailEl = document.querySelector('input#emailInput') as null|HTMLInputElement
+        if(!emailEl) return
+        const email = emailEl.value.trim()
+        if(email.length == 0 || !isValidEmail(email)) return
+        fetch('/api/account/email', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email })
+        }).then(res => {
+            if(res.ok) changePageToUrl('/account/')
+            else console.error(res.status)
+        })
+    })
+}
+
+/**
  * Loads page from another URL
  * @param url Page URL
  * @param pushState Add to window history
@@ -48,6 +113,7 @@ const parseAccountPage = (html: Element): void => {
 const changePageToUrl = async (url: string, pushState: boolean = true): Promise<void> => {
     if(page_loading) return
     const current_url = location.href // In case of an error while downloading page
+    cancelPreferencesPopup()
     startPageLoading()
     changeMenuButton(url)
     const page = await downloadPage(url)
@@ -59,6 +125,7 @@ const changePageToUrl = async (url: string, pushState: boolean = true): Promise<
     }
     if(pushState) window.history.pushState({ url }, '', url)
     parseAccountPage(html)
+    attachEventsOnPageLoad()
     stopPageLoading()
     return
 }
@@ -71,7 +138,7 @@ const changeMenuButton = (url: string): void => {
     const menu = document.querySelector('div#sideMenu')
     menu.querySelectorAll('a.side-menu-item').forEach(item => {
         const button = item as HTMLAnchorElement
-        if(button.href == url) return button.classList.add('active')
+        if(button.href == url || (url.includes('/edit/') && button.href.endsWith('/account/'))) return button.classList.add('active')
         button.classList.remove('active')
     })
 }
@@ -86,6 +153,7 @@ window.addEventListener('popstate', e => {
 addEventListener('load', e => {
     const url = location.href.endsWith('/') ? location.href : `${location.href}/`
     window.history.replaceState({ url }, '', url)
+    attachEventsOnPageLoad()
 })
 
 // Side Menu buttons onclick event
@@ -95,3 +163,96 @@ document.querySelectorAll('a.side-menu-item').forEach((el) => el.addEventListene
     const page_url = element.href
     changePageToUrl(page_url)
 }))
+
+// SETTINGS PAGE
+let show_again = null
+const HIDE_ANIMATION_TIME = 500
+/**
+ * Show preference popup
+ */
+const showPreferencesPopup = () => {
+    if(saving_preferences) setTimeout(showPreferencesPopup, 25)
+    document.querySelector("#saveChangesPopup").classList.add('active')
+}
+/**
+ * Close preference popup
+ */
+const closePreferencesPopup = () => {
+    const popup = document.querySelector("div#saveChangesPopup") as HTMLDivElement
+    if(!popup || !popup.classList.contains('active')) return
+    setTimeout(() => {
+        popup.classList.remove('active')
+        popup.style.animation = ''
+        saving_preferences = false
+    }, HIDE_ANIMATION_TIME)
+    popup.style.animation = 'bounceHide .5s ease'
+}
+
+/**
+ * Reset preferences to original value
+ */
+const resetPreferences = () => {
+    const secEmails = document.querySelector('input[data-name="security-emails"') as HTMLInputElement
+    const newsEmails = document.querySelector('input[data-name="newsletter-emails"') as HTMLInputElement
+    const saveDwnlds = document.querySelector('input[data-name="save-downloads"') as HTMLInputElement
+    if(newsEmails) newsEmails.checked = original_preferences.newsletter_emails
+    if(secEmails) secEmails.checked = original_preferences.security_emails
+    if(saveDwnlds) saveDwnlds.checked = original_preferences.save_downloads
+    current_preferences = ({ 
+        ...original_preferences
+    })
+}
+/**
+ * Prefrence change value event
+ * @param element 
+ */
+const preferenceChange = (element: HTMLInputElement): void => {
+    if(!element.dataset.name) return
+    const name = element.dataset.name
+    const value = element.checked
+    if(name === 'security-emails') current_preferences.security_emails = value
+    if(name === 'newsletter-emails') current_preferences.newsletter_emails = value
+    if(name === 'save-downloads') current_preferences.save_downloads = value
+    showPreferencesPopup()
+}
+let saving_preferences = false
+/**
+ * Save prefrence - API call
+ */
+const savePreferences = () => {
+    if(saving_preferences) return
+    saving_preferences = true
+    // start loading
+    const button = document.querySelector('button#preferenceSaveButton')
+    button.classList.add('loading')
+    const temp = {...current_preferences}
+    fetch('https://getinsta.xyz/api/account/settings', {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            newsletter_emails: String(temp.newsletter_emails),
+            security_emails: String(temp.security_emails),
+            save_downloads: String(temp.save_downloads)
+        })
+    }).then(res => {
+        button.classList.remove('loading')
+        if(!res.ok) {
+            resetPreferences()
+            closePreferencesPopup()
+            return
+        }
+        const HOLD_TIME = 1000
+        setTimeout(closePreferencesPopup, HOLD_TIME)
+        button.classList.add('saved')
+        setTimeout(() => button.classList.remove('saved'), HIDE_ANIMATION_TIME + HOLD_TIME)
+        original_preferences.newsletter_emails = temp.newsletter_emails
+        original_preferences.security_emails = temp.security_emails
+        original_preferences.save_downloads = temp.save_downloads
+    }).catch(error => null)
+}
+const cancelPreferencesPopup = () => {
+    resetPreferences()
+    closePreferencesPopup()
+}
